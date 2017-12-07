5bb0feb80b5c5a697fce0e40ae523e2702b939a2
siilak <marko@siilak.com>
2017-12-07 12:01:14 +0200

    Fix logo

---
+++ .history/docs/app/components/index_20171207120033.js
@@ -0,0 +1,27 @@
+/* Components */
+import Vue from 'vue'
+import PageContainer from './PageContainer'
+import SplashContainer from './SplashContainer'
+import LogoTele2Material from './LogoTele2Material'
+import CodeLoading from './CodeLoading'
+import CodeBlock from './CodeBlock'
+import CodeExample from './CodeExample'
+import CodepenEdit from './CodepenEdit'
+import GridLayout from './GridLayout'
+import GridLayoutItem from './GridLayoutItem'
+import ApiItem from './ApiItem'
+import ApiTable from './ApiTable/ApiTable'
+import NoteBlock from './NoteBlock/NoteBlock'
+
+Vue.component(PageContainer.name, PageContainer)
+Vue.component(SplashContainer.name, SplashContainer)
+Vue.component(LogoTele2Material.name, LogoTele2Material)
+Vue.component(CodeLoading.name, CodeLoading)
+Vue.component(CodeBlock.name, CodeBlock)
+Vue.component(CodeExample.name, CodeExample)
+Vue.component(CodepenEdit.name, CodepenEdit)
+Vue.component(GridLayout.name, GridLayout)
+Vue.component(GridLayoutItem.name, GridLayoutItem)
+Vue.component(ApiItem.name, ApiItem)
+Vue.component(ApiTable.name, ApiTable)
+Vue.component(NoteBlock.name, NoteBlock)
